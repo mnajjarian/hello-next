261a2ff0c3f747e6d040e9957cb3ae24f310a472
Mahdi Najjarian <mnajarian@gmail.com>
2019-08-23 13:47:50 +0300

    getInitialProps and fetch

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,14 +1,23 @@
 import { useRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../../components/MyLayout';
 
-const Post = () => {
-    const router = useRouter();
-    return(
-        <Layout>
-            <h1>{router.query.id}</h1>
-            <p>This is the blog post content.</p>
-        </Layout>
-    )
-};
+const Post = (props) => (
+    <Layout>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium} />
+    </Layout>
+);
+
+Post.getInitialProps = async (context) => {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+    return { show }
+}
+
 
 export default Post;
